76fdea06b41926a945467e1ee6ffb02bc4361c39
Sancaktar51 <73063520+Sancaktar51@users.noreply.github.com>
2020-12-08 22:47:29 +0300

    Update GirilenMetniBuyukVeKucukHarflerleYazdirma.py

--- GirilenMetniBuyukVeKucukHarflerleYazdirma.py
+++ GirilenMetniBuyukVeKucukHarflerleYazdirma.py
@@ -1,9 +1,5 @@
-#import time
-
 metin = input("Lütfen bir metin giriniz: ")
 print("\nGirilen metnin büyük harflerle yazılışı: ",metin.upper())
 print("\nGirilen metnin küçük harflerle yazılışı: ",metin.lower())
 
 input("\nProgramı bitirmek için ENTER tuşuna basınız...")
-
-#time.sleep(7)
