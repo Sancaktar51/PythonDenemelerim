a7e8a6181e797468eb1ec972750663ce1a98312f
Ali Rıza <alieke51@hotmail.com>
2020-12-08 22:42:36 +0300

    Create GirilenMetniBuyukVeKucukHarflerleYazdirma.py

---
+++ GirilenMetniBuyukVeKucukHarflerleYazdirma.py
@@ -0,0 +1,9 @@
+#import time
+
+metin = input("Lütfen bir metin giriniz: ")
+print("\nGirilen metnin büyük harflerle yazılışı: ",metin.upper())
+print("\nGirilen metnin küçük harflerle yazılışı: ",metin.lower())
+
+input("\nProgramı bitirmek için ENTER tuşuna basınız...")
+
+#time.sleep(7)
